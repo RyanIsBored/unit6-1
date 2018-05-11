3daf205eae8a05c1057dfed0ce607db272ba4291
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-05-11 08:40:16 -0400

    Updated from Brython Server: 5/11/2018 8:40:15 AM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,10 @@
+#Ryan Jones
+#5/11/18
 
+file = open('engmix.txt')
+
+numWords = 0
+for line in file:
+    line=line.strip()
+    if len(line)>0 and line[0]=='r' and line[-1]=='j':
+        print(line)
