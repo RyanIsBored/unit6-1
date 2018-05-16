c4cb754bdc9eb7608698b70956e0a1d12f645cb8
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-05-16 08:07:24 -0400

    Updated from Brython Server: 5/16/2018 8:07:23 AM

--- testing.py
+++ testing.py
@@ -1,1 +1,14 @@
+file = open('engmix.txt')
 
+word = input('enter a word: ')
+
+inD = False
+for line in file:
+    line = line.strip()
+    if word == line:
+        print('in dictionary')
+        inD = True
+        break
+
+if not inD:
+    print('no')
