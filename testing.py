cc6f0798ab1b2439752707a4390babf8ebe10847
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-05-16 08:15:39 -0400

    Updated from Brython Server: 5/16/2018 8:15:38 AM

--- testing.py
+++ testing.py
@@ -12,3 +12,39 @@ for line in file:
 
 if not inD:
     print('no')
+    
+  
+    
+
+file = open('engmix.txt')
+num = int(input('enter a number: ')
+
+L = []
+
+for line in file:
+    line = line.strip()
+    L.append(line)
+    
+print(L[num-1])
+
+
+file = open('lastWordDemo.py')
+
+for line in file:
+    print(line.strip()+ ('!')
+
+
+
+file = open('engmix.txt')
+letter = input('Enter a letter: ')
+
+mostchar = ''
+for line in file:
+    line = line.strip()
+    if line.count(letter) > mostchar.count(letter):
+        mostchar = line
+
+print(mostchar)
+
+
+
