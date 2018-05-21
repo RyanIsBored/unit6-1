092182abeecf72581e6479a86afbc994c723d895
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-05-20 22:58:10 -0400

    Updated from Brython Server: 5/20/2018 10:58:09 PM

--- confused.py
+++ confused.py
@@ -1,10 +1,14 @@
-#word.strip()
-'''
+
 file = open('engmix.txt')
 
+#
+'''
 for word in file:
     if word.strip()[0]=='z' and word.strip()[-1]=='n':
         print(word.strip())
         break
 '''
 
+for word in file:
+    if word.count('q')==2:
+        print(word.strip())
