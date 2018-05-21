181a1d1429375e0d07d8a527831c54e34df93757
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-05-21 08:16:11 -0400

    Updated from Brython Server: 5/21/2018 8:16:10 AM

--- unit6quiz.py
+++ unit6quiz.py
@@ -2,8 +2,30 @@
 #5/21/18
 
 file = open('engmix.txt')
+
+
+#program #1
+'''
 letter = input('enter a letter: ')
 
 for word in file:
     if word.count(letter)==4:
+        print(word.strip())
+'''
+
+
+#program #3
+'''
+number = int(input('enter a number: '))
+letter = input('enter a letter: ')
+for word in file:
+    if word.strip()[0]==letter and len(word.strip())==number:
+        print(word.strip())
+'''        
+
+wcount = 0
+for word in file:
+    if len(word.strip())>=10:
+        wcount+=1
+    if wcount==8000:
         print(word.strip())
